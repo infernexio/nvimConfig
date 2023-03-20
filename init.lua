486b2d91ae11848abaefc4bedaf92fa4892e525e
infernexio <sohail382517@gmail.com>
2023-03-20 13:33:18 -0400

    first commit

---
+++ init.lua
@@ -0,0 +1,13 @@
+-- setting up packer and plugins
+require("sohail.plugins-setup")
+
+-- core
+require("sohail.core.options")
+require("sohail.core.keymaps")
+require("sohail.core.colorscheme")
+
+-- plugins
+require("sohail.plugins.comment")
+require("sohail.plugins.nvim-tree")
+require("sohail.plugins.lualine")
+require("sohail.plugins.telescope")
