5541ccc7c9eb7061464098eb4f2d99b30ac5856b
infernexio <sohail382517@gmail.com>
2023-03-21 11:30:20 -0400

    added github copilot

--- lua/sohail/plugins-setup.lua
+++ lua/sohail/plugins-setup.lua
@@ -98,6 +98,7 @@ return packer.startup(function(use)
 	use("lewis6991/gitsigns.nvim") -- show line modifications on left hand side
 
 	use("mbbill/undotree")
+
 	if packer_bootstrap then
 		require("packer").sync()
 	end
